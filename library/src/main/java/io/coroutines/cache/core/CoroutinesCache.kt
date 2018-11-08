b80e2b0c6b75e6e5136c100f21e29ee3e4b8f776
diefferson.santos <diefferson.santos@gft.com>
2018-11-08 15:42:01 -0200

    verison 0.0.5

--- library/src/main/java/io/coroutines/cache/core/CoroutinesCache.kt
+++ library/src/main/java/io/coroutines/cache/core/CoroutinesCache.kt
@@ -31,7 +31,7 @@ class CoroutinesCache(private var context: Context): CoroutineScope{
         }
     }
 
-    suspend inline fun <reified T:Any> getFromSource(source:Deferred<T>, key: String):Deferred<T>{
+    fun <T> getFromSource(source:Deferred<T>, key: String):Deferred<T>{
         return async {
             val result = source.await()
             getDataBase().insert(Cache(key, Gson().toJson(result)))
