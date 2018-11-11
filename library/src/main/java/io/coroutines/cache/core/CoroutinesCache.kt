241b8441ed686689dc02bc12134966116a4771a5
Diefferson Santos <diefferson.sts@gmail.com>
2018-11-11 16:19:49 -0200

    Correction exception

--- library/src/main/java/io/coroutines/cache/core/CoroutinesCache.kt
+++ library/src/main/java/io/coroutines/cache/core/CoroutinesCache.kt
@@ -20,7 +20,7 @@ open class CoroutinesCache(private var context: Context): CoroutineScope{
 
     fun getDataBase() =  Room.databaseBuilder(context, LocalDatabase::class.java, context.packageName+CACHE_PREFIX).build().cacheDao()
 
-    inline fun <reified T:Any> asyncCache(noinline source: suspend ()->T, key: String, forceSource:Boolean): Deferred<T> {
+    inline fun <reified T:Any> asyncCache(noinline source: suspend ()->Deferred<T>, key: String, forceSource:Boolean): Deferred<T> {
         return if (forceSource) {
             getFromSource(source, key)
         } else {
@@ -32,24 +32,19 @@ open class CoroutinesCache(private var context: Context): CoroutineScope{
         }
     }
 
-    fun <T> getFromSource(source: suspend ()->T, key: String):Deferred<T>{
+    fun <T> getFromSource(source: suspend ()->Deferred<T>, key: String):Deferred<T>{
         return async {
-            val response = source()
-            getDataBase().insert(Cache(key, Gson().toJson(response)))
-            response
+            val result = source().await()
+            getDataBase().insert(Cache(key, Gson().toJson(result)))
+            result
         }
     }
 
     inline fun <reified T:Any> getFromCache(key: String): T? {
         val resultDb = getDataBase().get(key)?.data
         return if (resultDb != null) {
-            try {
-                val listType = object : TypeToken<T>() {}.type
-                Gson().fromJson(resultDb, listType) as T
-            }catch (e:StackOverflowError){
-                Gson().fromJson<T>(resultDb, T::class.java) as T
-            }
-
+            val listType = object : TypeToken<T>() {}.type
+            Gson().fromJson(resultDb, listType) as T
         } else {
             return null
         }
@@ -59,4 +54,3 @@ open class CoroutinesCache(private var context: Context): CoroutineScope{
         private const val CACHE_PREFIX = "cache"
     }
 }
-
